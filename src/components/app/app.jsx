09835f54fa818d3629a51d75684a737b6c611db5
Kornerupin <kornerupin@yandex.ru>
2019-05-05 00:08:38 +0300

    Added onclick props

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -7,12 +7,14 @@ const App = (props) => {
   return <WelcomeScreen
     errorCount = {props.errorCount}
     time = {props.gameTime}
+    onclick={props.onclick}
   />;
 };
 
 App.propTypes = {
   gameTime: PropTypes.number.isRequired,
   errorCount: PropTypes.number.isRequired,
+  onclick: PropTypes.func,
 };
 
 export default App;
--- src/components/welcome-screen/welcome-screen.test.js
+++ src/components/welcome-screen/welcome-screen.test.js
@@ -3,11 +3,13 @@ import renderer from "react-test-renderer";
 import WelcomeScreen from "./welcome-screen";
 
 it(`Save view`, () => {
+  const clickHandler = () => {};
   const tree = renderer
   .create(
       <WelcomeScreen
         time={5}
         errorCount={3}
+        onclick={clickHandler}
       />
   )
   .toJSON();
