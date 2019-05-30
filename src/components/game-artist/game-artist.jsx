5969bf0d81574ba30ec4d35db6d5be2221c59ee4
Andrey Braylovskiy <kornerupin@yandex.ru>
2019-05-30 22:37:56 +0300

    Try transform function to class

--- src/components/game-artist/game-artist.jsx
+++ src/components/game-artist/game-artist.jsx
@@ -1,77 +1,96 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const GameArtist = (props) => {
-  const data = props.data;
-  const onSetAnswer = props.onSetAnswer;
+class GameArtist extends React.Component {
+  constructor(props) {
+    super(props);
 
-  const result = {
-    'answer-1': false,
-    'answer-2': false,
-    'answer-3': false,
-    'answer-4': false,
-  };
+    this.state = {
+      result: {
+        'answer-1': false,
+        'answer-2': false,
+        'answer-3': false,
+        'answer-4': false,
+      },
+    };
 
-  const getUserAnswer = (evt) => {
-    result[evt.currentTarget.getAttribute(`id`)] = evt.currentTarget.checked;
-    onSetAnswer(result);
-  };
+    this.setUserAnswer = this.setUserAnswer.bind(this);
+  }
 
-  const artists = data.artists.map((artist, key) =>
-    <div className="artist" key={key}>
-      <input className="artist__input visually-hidden" onClick={getUserAnswer}
-        type="radio" name="answer" value={`artist-` + (key + 1)} id={`answer-` + (key + 1)} />
-      <label className="artist__name" htmlFor={`answer-` + (key + 1)}>
-        <img className="artist__picture" src={artist.img} alt={artist.name} />
-        {artist.name}
-      </label>
-    </div>
-  );
+  setUserAnswer(evt) {
+    this.setState({
+      result: {
+        [evt.currentTarget.id]: evt.currentTarget.checked
+      },
+    });
+    this.props.onSetAnswer(this.state.result);
+  }
 
-  return <section className="game game--artist">
-    <header className="game__header">
-      <a className="game__back" href="#">
-        <span className="visually-hidden">Сыграть ещё раз</span>
-        <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию" />
-      </a>
+  render() {
+    const data = this.props.data;
 
-      <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
-        <circle className="timer__line" cx="390" cy="390" r="370"
-          style={{
-            filter: `url(#blur)`,
-            transform: `rotate(-90deg) scaleY(-1)`,
-            transformOrigin: `center`,
-          }}/>
-      </svg>
-
-      <div className="timer__value" xmlns="http://www.w3.org/1999/xhtml">
-        <span className="timer__mins">05</span>
-        <span className="timer__dots">:</span>
-        <span className="timer__secs">00</span>
+    const artists = data.artists.map((artist, key) =>
+      <div className="artist" key={key}>
+        <input className="artist__input visually-hidden" onChange={this.setUserAnswer}
+          type="radio" name="answer" value={`artist-` + (key + 1)} id={`answer-` + (key + 1)} />
+        <label className="artist__name" htmlFor={`answer-` + (key + 1)}>
+          <img className="artist__picture" src={artist.img} alt={artist.name} />
+          {artist.name}
+        </label>
       </div>
+    );
 
-      <div className="game__mistakes">
-        <div className="wrong"></div>
-        <div className="wrong"></div>
-        <div className="wrong"></div>
-      </div>
-    </header>
 
-    <section className="game__screen">
-      <h2 className="game__title">Кто исполняет эту песню?</h2>
-      <div className="game__track">
-        <button className="track__button track__button--play" type="button"></button>
-        <audio src={data.src}></audio>
-      </div>
+    return <section className="game game--artist">
+      <header className="game__header">
+        <a className="game__back" href="#">
+          <span className="visually-hidden">Сыграть ещё раз</span>
+          <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию" />
+        </a>
 
-      <form className="game__artist">
+        <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
+          <circle className="timer__line" cx="390" cy="390" r="370"
+            style={{
+              filter: `url(#blur)`,
+              transform: `rotate(-90deg) scaleY(-1)`,
+              transformOrigin: `center`,
+            }}/>
+        </svg>
 
-        {artists}
+        <div className="timer__value" xmlns="http://www.w3.org/1999/xhtml">
+          <span className="timer__mins">05</span>
+          <span className="timer__dots">:</span>
+          <span className="timer__secs">00</span>
+        </div>
 
-      </form>
-    </section>
-  </section>;
-};
+        <div className="game__mistakes">
+          <div className="wrong"></div>
+          <div className="wrong"></div>
+          <div className="wrong"></div>
+        </div>
+      </header>
+
+      <section className="game__screen">
+        <h2 className="game__title">Кто исполняет эту песню?</h2>
+        <div className="game__track">
+          <button className="track__button track__button--play" type="button"></button>
+          <audio src={data.src}></audio>
+        </div>
+
+        <form className="game__artist">
+
+          {artists}
+
+        </form>
+      </section>
+    </section>;
+  }
+}
+
+// const getUserAnswer = (evt) => {
+//   result[evt.currentTarget.getAttribute(`id`)] = evt.currentTarget.checked;
+//   onSetAnswer(result);
+// };
 
 GameArtist.propTypes = {
   data: PropTypes.object.isRequired,
